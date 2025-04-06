b2a3f7b3d1ef39281aef9d87b8976dce66b762b6
윤설이 <olerlmin1234@gmail.com>
2025-04-06 09:15:05 +0900

    Initial commit

---
+++ src/main/java/com/seol/snibby/SnibbyApplication.java
@@ -0,0 +1,13 @@
+package com.seol.snibby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnibbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnibbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seol/snibby/SnibbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seol.snibby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnibbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

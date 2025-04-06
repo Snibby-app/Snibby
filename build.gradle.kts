64aced51bdd5568ceb9b0349ea0e09c45e213164
윤설이 <olerlmin1234@gmail.com>
2025-04-06 09:45:30 +0900

    build.gradle.kts 추가, 도커에 Mongodb, mysql, redis 설정하고 올리기

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("java-test-fixtures")
+    id("jacoco")
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "com.seol"
@@ -9,7 +12,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -17,17 +20,57 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    create("intTest") {
+        java {
+            setSrcDirs(listOf("src/intTest/java"))
+        }
+        compileClasspath += sourceSets["main"].output + sourceSets["test"].output
+        runtimeClasspath += output + compileClasspath
+    }
+}
+
+configurations["intTestImplementation"].extendsFrom(configurations["testImplementation"])
+configurations["intTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
+
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    runtimeOnly("mysql:mysql-connector-java:8.0.32")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
+    testRuntimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring3x:4.16.1")
+
+    add("intTestImplementation", "com.github.codemonstur:embedded-redis:1.4.3")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.register<Test>("intTest") {
+    description = "Runs integration tests"
+    group = "verification"
+    testClassesDirs = sourceSets["intTest"].output.classesDirs
+    classpath = sourceSets["intTest"].runtimeClasspath
+    shouldRunAfter(tasks.test)
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    afterEvaluate {
+        classDirectories.setFrom(
+            files(classDirectories.files.map {
+                fileTree(it).matching {
+                    exclude("**/config/*")
+                }
+            })
+        )
+    }
 }
